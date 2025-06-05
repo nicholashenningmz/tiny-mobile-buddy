a01ce8f1a91152bd93b2330e1e77fedded87eda5
nicholashenningmz <157512411+nicholashenningmz@users.noreply.github.com>
2025-06-05 15:10:42 -0400

    Update capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,14 +1,12 @@
 
+
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'app.lovable.54c6c277eebe416ea27639ced66386a0',
-  appName: 'tiny-mobile-buddy',
+  appId: 'com.phoenicks.evilchoozi',
+  appName: 'Evil Choozi',
   webDir: 'dist',
-  server: {
-    url: 'https://54c6c277-eebe-416e-a276-39ced66386a0.lovableproject.com?forceHideBadge=true',
-    cleartext: true
-  },
+
   bundledWebRuntime: false
 };
 
